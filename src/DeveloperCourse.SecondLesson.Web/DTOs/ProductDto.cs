956ef1d830fcdc5ce3e5de1e34e0a7f04f1c0170
Veniamin Zakharov <salodara@gmail.com>
2020-10-31 00:30:36 +0300

    + add web

---
+++ src/DeveloperCourse.SecondLesson.Web/DTOs/ProductDto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace DeveloperCourse.ThirdLesson.View.DTOs
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Sku { get; set; }
+
+        public string Weight { get; set; }
+
+        public IEnumerable<ImageDto> Images { get; set; }
+
+        public IEnumerable<PriceDto> Prices { get; set; }
+    }
+}
