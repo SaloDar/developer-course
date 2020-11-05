4cf8083fd7247990720dabdb1b0cbe614b628700
Veniamin Zakharov <salodara@gmail.com>
2020-11-05 17:52:43 +0300

    + add shared project

---
+++ src/DeveloperCourse.SecondLesson.Shared/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DeveloperCourse.SecondLesson.Shared.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public Guid CreatedBy { get; set; }
+
+        public DateTime LastSavedDate { get; set; }
+
+        public Guid LastSavedBy { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
