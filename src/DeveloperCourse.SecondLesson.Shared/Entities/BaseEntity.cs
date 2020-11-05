1c766ae1bb2e3ab604602e9bdab115588b0e3b73
Veniamin Zakharov <salodara@gmail.com>
2020-11-05 18:50:37 +0300

    = add constructor and change props access modifiers

--- src/DeveloperCourse.SecondLesson.Shared/Entities/BaseEntity.cs
+++ src/DeveloperCourse.SecondLesson.Shared/Entities/BaseEntity.cs
@@ -4,16 +4,20 @@ namespace DeveloperCourse.SecondLesson.Shared.Entities
 {
     public class BaseEntity
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; protected set; }
 
-        public DateTime CreatedDate { get; set; }
-
-        public Guid CreatedBy { get; set; }
-
-        public DateTime LastSavedDate { get; set; }
-
-        public Guid LastSavedBy { get; set; }
-
-        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate { get; protected set; }
+        
+        public DateTime LastSavedDate { get; protected set; }
+        
+        public bool IsDeleted { get; protected set; }
+        
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.UtcNow;
+            LastSavedDate = DateTime.UtcNow;
+            IsDeleted = false;
+        }
     }
 }
