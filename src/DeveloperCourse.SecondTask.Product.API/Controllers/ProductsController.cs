06c96111902cc363fb550d5634f148402ed436ab
Veniamin Zakharov <salodara@gmail.com>
2020-11-09 02:25:08 +0300

    + add documentation for product controller

--- src/DeveloperCourse.SecondTask.Product.API/Controllers/ProductsController.cs
+++ src/DeveloperCourse.SecondTask.Product.API/Controllers/ProductsController.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
+using DeveloperCourse.SecondTask.Infrastructure.Attributes;
 using DeveloperCourse.SecondTask.Product.API.Controllers.DTOs;
 using DeveloperCourse.SecondTask.Product.API.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -24,7 +27,15 @@ namespace DeveloperCourse.SecondTask.Product.API.Controllers
             _productService = productService;
         }
 
+        /// <summary>
+        /// Retrieves all products.
+        /// </summary>
+        /// <returns>Returns products</returns>
+        /// <response code="200">Returns products</response>
         [HttpGet]
+        [ProducesResponseType(typeof(GetProductsResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<GetProductsResponse> GetProducts()
         {
             var result = await _productService.GetProducts();
@@ -32,8 +43,16 @@ namespace DeveloperCourse.SecondTask.Product.API.Controllers
             return _mapper.Map<GetProductsResponse>(result);
         }
 
+        /// <summary>
+        /// Retrieves a specific product by unique id.
+        /// </summary>
+        /// <returns>Returns the product</returns>
+        /// <response code="200">Returns the product</response>
         [HttpGet("{id}")]
-        public async Task<GetProductResponse> GetProduct(GetProductRequest request)
+        [ProducesResponseType(typeof(GetProductResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<GetProductResponse> GetProduct([FromMultiSource] GetProductRequest request)
         {
             var result = await _productService.GetProduct(request.Id);
 
