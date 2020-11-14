c1f2a4b71eead1f61bc033a3a6bd0b9ce2185093
Veniamin Zakharov <salodara@gmail.com>
2020-11-14 17:51:51 +0300

    = fix docs and validation for dtos

--- src/DeveloperCourse.SecondTask.Price.API/Controllers/DTOs/UpdatePriceDto.cs
+++ src/DeveloperCourse.SecondTask.Price.API/Controllers/DTOs/UpdatePriceDto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using Money;
 
 namespace DeveloperCourse.SecondTask.Price.API.Controllers.DTOs
@@ -6,7 +7,7 @@ namespace DeveloperCourse.SecondTask.Price.API.Controllers.DTOs
     public class UpdatePriceDto
     {
         /// <summary>
-        /// Product identifier, for filter the prices.
+        /// Product identifier.
         /// </summary>
         /// <example>BAD0468C-9CA9-4179-A20D-1F9EEE74318B</example>
         public Guid? ProductId { get; set; } = null;
@@ -15,18 +16,21 @@ namespace DeveloperCourse.SecondTask.Price.API.Controllers.DTOs
         /// Retail price.
         /// </summary>
         /// <example>100</example>
+        [Range(0, double.MaxValue)]
         public decimal? RetailPrice { get; set; } = null;
 
         /// <summary>
         /// Cost price
         /// </summary>
         /// <example>200</example>
+        [Range(0, double.MaxValue)]
         public decimal? CostPrice { get; set; } = null;
 
         /// <summary>
         /// Currency.
         /// </summary>
         /// <example>643</example>
+        [EnumDataType(typeof(Currency))]
         public Currency? Currency { get; set; } = null;
     }
 }
