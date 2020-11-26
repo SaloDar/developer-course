a58cfef5826874582ab552310de89865dd1fd730
Veniamin Zakharov <salodara@gmail.com>
2020-11-26 16:33:21 +0300

    = refactor forward token handler

--- src/DeveloperCourse.SecondLesson.Common.Clients/MessageHandlers/ForwardAuthenticateTokenMessageHandler.cs
+++ src/DeveloperCourse.SecondLesson.Common.Clients/MessageHandlers/ForwardAuthenticateTokenMessageHandler.cs
@@ -9,12 +9,28 @@ namespace DeveloperCourse.SecondLesson.Common.Clients.MessageHandlers
 {
     public class ForwardAuthenticateTokenMessageHandler : DelegatingHandler
     {
+        #region Consts
+
+        private const string AuthorizationKey = "Authorization";
+
+        #endregion
+        
+        #region Fields
+
         private readonly IHttpContextAccessor _httpContextAccessor;
+
+        #endregion
+
+        #region Constructors
 
         public ForwardAuthenticateTokenMessageHandler(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
         }
+
+        #endregion
+
+        #region Ovveriden Methods
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
@@ -23,18 +39,20 @@ namespace DeveloperCourse.SecondLesson.Common.Clients.MessageHandlers
             if (auth != null)
             {
                 var tokenWithScheme = _httpContextAccessor.HttpContext.Request.Headers
-                    .FirstOrDefault(x => x.Key.Equals("Authorization", StringComparison.OrdinalIgnoreCase))
+                    .FirstOrDefault(x => x.Key.Equals(AuthorizationKey, StringComparison.OrdinalIgnoreCase))
                     .Value
                     .FirstOrDefault(y => y.StartsWith(auth.Scheme, StringComparison.OrdinalIgnoreCase));
 
                 if (!string.IsNullOrWhiteSpace(tokenWithScheme))
                 {
-                    request.Headers.Remove("Authorization");
-                    request.Headers.Add("Authorization", tokenWithScheme);
+                    request.Headers.Remove(AuthorizationKey);
+                    request.Headers.Add(AuthorizationKey, tokenWithScheme);
                 }
             }
             
             return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
         }
+
+        #endregion
     }
 }
