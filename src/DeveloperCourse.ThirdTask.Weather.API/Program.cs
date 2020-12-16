4b4c34cc98076ae282e3cb6df56e2b06f24dc340
Veniamin Zakharov <salodara@gmail.com>
2020-12-17 01:27:42 +0300

    + add environment variables

--- src/DeveloperCourse.ThirdTask.Weather.API/Program.cs
+++ src/DeveloperCourse.ThirdTask.Weather.API/Program.cs
@@ -24,6 +24,7 @@ namespace DeveloperCourse.ThirdTask.Weather.API
                     config.SetBasePath(Directory.GetCurrentDirectory());
                     config.AddJsonFile($"appsettings.json", false);
                     config.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true);
+                    config.AddEnvironmentVariables("DC_Weather_");
                     config.AddCommandLine(args);
                 })
                 .ConfigureLogging((hostingContext, logging) =>
